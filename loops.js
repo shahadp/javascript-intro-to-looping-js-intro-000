767f0c179fb9fdaf759911816957fe721a1a4b77
Shahad Puthukudi <shahadp@gmail.com>
2020-05-23 00:56:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,8 +2,8 @@ function forLoop(array)
 {
 for (let i=0;i<25;i++)
 {
-console.log(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
-array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+console.log(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
 }
 return array
 }
