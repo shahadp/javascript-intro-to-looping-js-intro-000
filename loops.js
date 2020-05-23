4d7ca785ebe5914be661739bba3950c584cbc0a9
Shahad Puthukudi <shahadp@gmail.com>
2020-05-23 00:46:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,5 @@
-loops
-    forLoop(array)
-      1) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
-      to an array 25 times
+
 
 let array=[]
 for (let i=0;i<25;i++)
+array.push[`"I am ${i} strange loop${i === 0 ? '' : 's'}."`]
